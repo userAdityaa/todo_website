527ab785ba785518c212b07ad6e7bafdb189d521
userAdityaa <aditya.chaudhary1558@gmail.com>
2024-12-25 22:45:39 +0545

    pages for the menu home item will be added.

---
+++ app/pages/TodayPage.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { Public_Sans } from 'next/font/google';
+
+const public_sans = Public_Sans({
+  subsets: ['latin'],
+  weight: '400',
+});
+
+const TodayPage = () => {
+  const [userTask, setUserTask] = useState("");
+  const [list, setList] = useState<{ task: string; completed: boolean }[]>([]);
+
+  const handleAddTask = () => {
+    if (userTask.trim()) {
+      setList((prevList) => [
+        ...prevList,
+        { task: userTask.trim(), completed: false },
+      ]);
+      setUserTask("");
+    }
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleAddTask();
+    }
+  };
+
+  const toggleTaskCompletion = (index: number) => {
+    setList((prevList) =>
+      prevList.map((item, i) =>
+        i === index ? { ...item, completed: !item.completed } : item
+      )
+    );
+  };
+
+  return (
+    <div className={`flex flex-col text-black ${public_sans.className} px-4`}>
+      <h1 className="font-bold text-[40px]">Today</h1>
+      <div className="flex items-center border border-gray-300 px-2 py-1 mt-[1.5rem] rounded-lg">
+        <img
+          src="/images/plus_sign.svg"
+          alt="Add task"
+          className="w-4 h-4 mr-2 cursor-pointer"
+          onClick={handleAddTask}
+        />
+        <input
+          type="text"
+          placeholder="Add new task"
+          className="flex-grow outline-none p-2"
+          value={userTask}
+          onChange={(e) => setUserTask(e.target.value)}
+          onKeyDown={handleKeyPress}
+        />
+      </div>
+      <ul className="mt-4">
+        {list.map((item, index) => (
+          <li
+            key={index}
+            className="flex items-center gap-3 border-b border-gray-300 py-2  hover:cursor-pointer"
+          >
+            <input
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => toggleTaskCompletion(index)}
+              className="cursor-pointer"
+            />
+            <span
+              className={`flex-grow ${
+                item.completed ? "line-through text-gray-500" : ""
+              }`}
+            >
+              {item.task}
+            </span>
+            <img
+              src="/images/arrow_right.svg"
+              alt="More details"
+              className="w-4 h-4 cursor-pointer"
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodayPage;
